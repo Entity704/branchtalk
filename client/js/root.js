880308d4c4b05d7f2d38fc52a79c6ad48ce2474d
Entity704 <EntityMC@outlook.com>
2025-11-19 22:08:31 +0800

    feat: add header to show root node infos; realize create node function

--- client/js/root.js
+++ client/js/root.js
@@ -4,8 +4,13 @@ import * as ui from './ui.js';
 const params = new URLSearchParams(location.search);
 
 const isCreate = params.get('create') === '1';
-const rootId = params.get('id');
-const rootData = await api.getRoot(rootId)
+let rootId, rootData;
+if(!isCreate) {
+    rootId = params.get('id');
+    rootData = await api.getRoot(rootId);
+}
+
+const nodeTree = document.getElementById('node-tree');
 
 const isRootRadio = document.getElementById('isRoot');
 const titleInput = document.getElementById('editor-title');
@@ -13,19 +18,38 @@ const tagsInput = document.getElementById('editor-tags');
 const input = document.getElementById('editor-textarea');
 const submit = document.getElementById('submit');
 
+let createNodeParents = [];
+
+const createPage = {
+    root_node: {
+        title: 'Create New Node',
+        timestamp: Date.now() / 1000,
+        tags: [],
+        parents: [],
+        content: 'You can create a new branch tree (root node) on this page'
+    },
+    nodes: []
+}
+
 /* --- show nodes --- */
 
 function renderTree(rootData) {
+    document.getElementById('nodes').innerHTML = '';
+
+    document.getElementById('title').textContent = rootData.root_node.title;
+    document.getElementById('last-updated').textContent = (new Date(rootData.root_node.timestamp * 1000).toLocaleString());
+    document.getElementById('content').textContent = rootData.root_node.content;
+
     // TODO: complete this
 }
 
 function renderCreateTree() {
-    // TODO: complete this
+    renderTree(createPage);
 }
 
 /* --- handles --- */
 
-function handleSubmit() {
+async function handleSubmit() {
     let res;
 
     const title = titleInput.value;
@@ -34,11 +58,18 @@ function handleSubmit() {
     if (isRootRadio.checked) {
         // TODO: implement this feature
     } else {
-        // TODO: implement this feature
+        res = await api.createNode(
+            {
+                rootid: rootId,
+                content: input.value,
+                parents: createNodeParents
+            }
+        );
     }
 
     if (res.ok) {
         input.value = '';
+        ui.notice('Submit succeed!', '#108610');
     } else {
         ui.notice('Submit failed!');
     }
